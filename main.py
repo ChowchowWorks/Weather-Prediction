416cda2af09efb7b1c7846310107da1517e0e9b5
ChowchowWorks <clarencechow1@outlook.com>
2025-03-18 13:37:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 import numpy as np
 import csv
+import helper as h
 
-def loadWeatherData(filename):
-    # read in the weather CSV into a np array
-    # return array
-    csv.lst = []
-    with open(filename, 'r') as file:
-        reader = csv.reader(file, delimiter= ';')
-        header = next(reader)
-        for row in reader:
-            csv.lst.append(row)
-    arr = np.array(csv.lst)
-    return arr
-
-data = loadWeatherData('weather_data.csv')
+data = h.loadWeatherData('weather_data.csv')
