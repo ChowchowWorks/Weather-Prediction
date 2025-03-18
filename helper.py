94b5be89c9785e9d80bd44e10cce4cd04e7560f4
ChowchowWorks <clarencechow1@outlook.com>
2025-03-18 13:51:14 +0800

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+import matplotlib.pyplot as plt
 
 def loadWeatherData(filename):
     # read in the weather CSV into a np array
@@ -8,7 +9,16 @@ def loadWeatherData(filename):
     with open(filename, 'r') as file:
         reader = csv.reader(file, delimiter= ';')
         header = next(reader)
+        print(header)
         for row in reader:
             csv.lst.append(row)
     arr = np.array(csv.lst)
     return arr
+
+def plothumidity(data):
+    y_val = data
+    time_steps = np.arange(len(data))
+    plt.plot(time_steps, y_val, label = 'reletive humidity', color = 'b', linestyle = '-')
+    plt.xlabel("Time Steps")
+    plt.ylabel("Humidity")
+    plt.title("Time Series Plot of Reletive Humidity")
